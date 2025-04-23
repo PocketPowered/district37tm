ec427c083abcf7b543be24cdc1607234231968a6
Chris Wong <wongislandd@gmail.com>
2025-04-23 17:28:21 -0400

    Setting up github pages

--- event-manager/src/firebase.ts
+++ event-manager/src/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -10,9 +9,9 @@ const firebaseConfig = {
   messagingSenderId: "117850780335",
   appId: "1:117850780335:web:7171bdb759ddc8b3d8da5a",
   measurementId: "G-4JTXWV37E1"
-}
+};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider(); 
+export const googleProvider = new GoogleAuthProvider();
