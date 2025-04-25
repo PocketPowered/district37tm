8c460769d625ddd01e40e81ad58163ded96465b0
Chris Wong <wongislandd@gmail.com>
2025-04-25 13:09:55 -0400

    bump version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -64,8 +64,8 @@ android {
         applicationId = "com.district37.toastmasters"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
     packaging {
         resources {
