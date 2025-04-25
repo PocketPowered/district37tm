e5da7eabea5f65af88fb8a07301d13722be0565e
Chris Wong <wongislandd@gmail.com>
2025-04-25 09:59:41 -0400

    updating default title

--- composeApp/src/commonMain/kotlin/com/wongislandd/nexus/navigation/GlobalTopAppBar.kt
+++ composeApp/src/commonMain/kotlin/com/wongislandd/nexus/navigation/GlobalTopAppBar.kt
@@ -27,7 +27,7 @@ fun GlobalTopAppBar(
     isTitleShown: Boolean = true,
     showBackButton: Boolean = true,
     homeDestination: String,
-    defaultTitle: String = "District 37 Conference - 2025",
+    defaultTitle: String = "D37 Conference - 2025",
     actions: (@Composable RowScope.() -> Unit) = {},
     onHamburgerMenuClick: () -> Unit = {},
     modifier: Modifier = Modifier
